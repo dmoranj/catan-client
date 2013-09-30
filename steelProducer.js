82f1f7cf201b933bff1e628459c319dd14c5b8ef
Daniel Moran <dmj@tid.es>
2013-09-30 14:41:02 +0200

    Finished steel consumer

--- steelProducer.js
+++ steelProducer.js
@@ -1,66 +1,82 @@
 var config = require("./config"),
-    request = require('request');
+    request = require('request'),
+    async = require("async");
 
-var socket = require('socket.io-client').connect(config.steelProducer.url + ":" + config.steelProducer.portSocketIO);
+function executeProtocol() {
+    console.log("Initiating interchange");
 
-socket.on('¡Hola Don Pepito!', function (data) {
-    socket.emit('¡Hola Don Jose!');
-});
+    var socket = require('socket.io-client').connect(config.steelProducer.url + ":" + config.steelProducer.portSocketIO);
 
-socket.on('¿Pasó usted por mi casa?', function (data) {
-    var options = {
-        url: config.steelProducer.url + ":" + config.steelProducer.portHttp + "/casa",
-        method: "POST",
-        json: {
-            address: data.address
-        }
-    };
-
-    request(options, function (error, response, body) {
-        if (error) {
-            console.log("Error: could not connect with the server");
-        } else if (response.statusCode == 200) {
-            socket.emit("Por su casa yo pasé", {
-                confirmation: body.certificate
-            });
-        } else {
-            console.log("Error: " + response.statusCode);
-        }
+    socket.on('¡Hola Don Pepito!', function (data) {
+        socket.emit('¡Hola Don Jose!');
     });
-});
 
-socket.on('¿Vio usted a mi abuela?', function (data) {
+    socket.on('¿Pasó usted por mi casa?', function (data) {
+        var options = {
+            url: config.steelProducer.url + ":" + config.steelProducer.portHttp + "/casa",
+            method: "POST",
+            json: {
+                address: data.address
+            }
+        };
 
-    console.log(data.grandMaName);
-
-    var options = {
-        url: config.steelProducer.url + ":" + config.steelProducer.portHttp + "/abuela",
-        method: "POST",
-        json: {
-            granMaName: data.grandMaName
-        }
-    };
-
-    request(options, function (error, response, body) {
-        if (error) {
-            console.log("Error: could not connect with the server");
-        } else if (response.statusCode == 200) {
-            socket.emit("A su abuela yo la vi", {
-                grandMaName: body.certificate
-            });
-        } else {
-            console.log("Error: " + response.statusCode);
-        }
+        request(options, function (error, response, body) {
+            if (error) {
+                console.log("Error: could not connect with the server");
+                socket.emit('¡Hola Don Jose!');
+            } else if (response.statusCode == 200) {
+                socket.emit("Por su casa yo pasé", {
+                    confirmation: body.certificate
+                });
+            } else {
+                console.log("Error: " + response.statusCode);
+                socket.emit('¡Hola Don Jose!');
+            }
+        });
     });
-});
 
-socket.on('¡Adios Don Pepito!', function (data) {
-    socket.emit("¡Adios Don Jose!");
+    socket.on('¿Vio usted a mi abuela?', function (data) {
+        var options = {
+            url: config.steelProducer.url + ":" + config.steelProducer.portHttp + "/abuela",
+            method: "POST",
+            json: {
+                granMaName: data.grandMaName
+            }
+        };
 
-    console.log("Resource: " + JSON.stringify(data));
-});
+        request(options, function (error, response, body) {
+            if (error) {
+                console.log("Error: could not connect with the server");
+            } else if (response.statusCode == 200) {
+                socket.emit("A su abuela yo la vi", {
+                    grandMaName: body.certificate
+                });
+            } else {
+                callback(null);
+                console.log("Error: " + body.message);
+            }
+        });
+    });
 
-socket.on('Error', function (data) {
-    console.log("An error occurred: " + data.message);
-    process.exit();
-})
+    socket.on('¡Adios Don Pepito!', function (data) {
+        socket.emit("¡Adios Don Jose!");
+
+        console.log("Resource: " + JSON.stringify(data));
+        socket.emit('¡Hola Don Jose!');
+    });
+
+    socket.on('Error', function (data) {
+        console.log("An error occurred: " + data.message);
+        socket.emit('¡Hola Don Jose!');
+    })
+}
+
+executeProtocol();
+
+
+
+
+
+
+
+
